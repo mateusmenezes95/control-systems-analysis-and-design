83a05ebc60e6da35c56bd98640a3934614925945
Mateus Menezes <mateusmenezes95@gmail.com>
2021-09-11 18:40:09 -0300

    Add controller output TFs

--- lib/common_functions_script.m
+++ lib/common_functions_script.m
@@ -1,18 +1,3 @@
-%% -*- texinfo -*-
-%% Robust control of a mass-damper-spring system.
-##
-%% Type @code{which MDSSystem} to locate,
-##
-%% @code{edit MDSSystem} to open and simply
-##
-%% @code{MDSSystem} to run the example file.
-
-% ===============================================================================
-% Robust Control of a Mass-Damper-Spring System     Lukas Reichlin    August 2011
-% ===============================================================================
-% Reference: Gu, D.W., Petkov, P.Hr. and Konstantinov, M.M.
-%            Robust Control Design with Matlab, Springer 2005
-
 printf("Loading commons functions...\n");
 
 pkg load control
@@ -56,22 +41,53 @@ function input_signal = get_signal (sim_time, start_time, end_time, amplitude)
     input_signal.signal = get_step_signal(sim_time, start_time, end_time, amplitude);
 end
 
+% Y(s)/R(s)
 function y_to_r_tf = get_output_to_reference_tf (G, C, F)
-    y_to_r_tf = minreal(F * ((C * G) / (1 + (C * G))));  % Y(s)/R(s)
+    y_to_r_tf = minreal(F * ((C * G) / (1 + (C * G))));
     y_to_r_tf.inname = 'R(s)';
     y_to_r_tf.outname = 'Y(s)';
 end
 
+% Y(s)/Qu(s)
 function y_to_qu_tf = get_output_to_input_disturbance_tf (G, C, F)
-    y_to_qu_tf = minreal(F * (G / (1 + (C * G))));               % Y(s)/Qu(s)
+    y_to_qu_tf = minreal(F * (G / (1 + (C * G))));
     y_to_qu_tf.inname = 'Qu(s)';
     y_to_qu_tf.outname = 'Y(s)';
 end
 
+% Y(s)/Qy(s)
 function y_to_qy_tf = get_output_to_output_disturbance_tf (G, C, F)
-    y_to_qy_tf = minreal(F * (1 / (1 + (C * G))));               % Y(s)/Qy(s)
+    y_to_qy_tf = minreal(F * (1 / (1 + (C * G))));
     y_to_qy_tf.inname = 'Qy(s)';
     y_to_qy_tf.outname = 'Y(s)';
+end
+
+% E(s)/R(s)
+function e_to_r_tf = get_error_to_reference (G, C, F)
+    e_to_r_tf = get_output_to_output_disturbance_tf(G, C, F);
+    e_to_r_tf.inname = 'R(s)';
+    e_to_r_tf.outname = 'E(s)';
+end
+
+ % U(s)/R(s)
+function u_to_r_tf = get_controller_output_to_reference_tf (G, C, F)
+    u_to_r_tf = minreal(F * (C / (1 + (C * G))));
+    u_to_r_tf.inname = 'R(s)';
+    u_to_r_tf.outname = 'U(s)';
+end
+
+ % U(s)/Qy(s)
+function u_to_qu_tf = get_controller_output_to_output_disturbance_tf (G, C, F)
+    u_to_qu_tf = minreal(((-1 * C) / (1 + (C * G))));
+    u_to_qu_tf.inname = 'Qy(s)';
+    u_to_qu_tf.outname = 'U(s)';
+end
+
+ % U(s)/Qu(s)
+function u_to_qu_tf = get_controller_output_to_input_disturbance_tf (G, C, F)
+    u_to_qu_tf = minreal(((-1 * C * G) / (1 + (C * G))));
+    u_to_qu_tf.inname = 'Qu(s)';
+    u_to_qu_tf.outname = 'U(s)';
 end
 
 printf("Loaded successfuly common functions \n");
--- scripts/challenge-1/question_1.m
+++ scripts/challenge-1/question_1.m
@@ -13,7 +13,7 @@ sim = get_sim_time (dt = 0.01, end_time = 50);
 % Input signals
 % ===============================================================================
 
-unit_step = get_signal (sim.time, start_time = 2, end_time = inf, amplitude = 1);
+reference = get_signal (sim.time, start_time = 2, end_time = inf, amplitude = 1);
 output_disturbance = get_signal (sim.time, start_time = 15, end_time = inf, amplitude = -0.2);
 input_disturbance = get_signal (sim.time, start_time = 25, end_time = inf, amplitude = -0.2);
 
@@ -34,24 +34,25 @@ y_to_r = get_output_to_reference_tf (G, C, F)
 y_to_qy = get_output_to_output_disturbance_tf (G, C, F)
 y_to_qu = get_output_to_input_disturbance_tf (G, C, F)
 
-unit_step.response = lsim(y_to_r, unit_step.signal, sim.time);
+u_to_r = get_controller_output_to_reference_tf (G, C, F)
+u_to_qy = get_controller_output_to_output_disturbance_tf (G, C, F)
+u_to_qu = get_controller_output_to_input_disturbance_tf (G, C, F)
+
+reference.response = lsim(y_to_r, reference.signal, sim.time);
 output_disturbance.response = lsim(y_to_qy, output_disturbance.signal, sim.time);
 input_disturbance.response = lsim(y_to_qu, input_disturbance.signal, sim.time);
 
-control_loop_response = unit_step.response + output_disturbance.response + input_disturbance.response;
+reference.controller_output = lsim(u_to_r, reference.signal, sim.time);
+output_disturbance.controller_output = lsim(u_to_qy, output_disturbance.signal, sim.time);
+input_disturbance.controller_output = lsim(u_to_qu, input_disturbance.signal, sim.time);
 
-nfont=15;   % define tamanho da fonte de texto
-nlinha=2;   % define espessura da linha
+control_loop_response = (reference.response +
+                         output_disturbance.response +
+                         input_disturbance.response);
 
-figure(1)
-
-subplot(2,1,1)
-plot(sim.time, unit_step.signal, '--r', 'linewidth', nlinha-0.5)
-hold on
-plot(sim.time, control_loop_response, 'b', 'linewidth', nlinha)
-grid on
-axis([0 sim.time(end) min(control_loop_response)-0.2  max(control_loop_response)+0.2])
-set(gca,'fontsize',nfont)
+controller_output_signal = (reference.controller_output +
+                            output_disturbance.controller_output +
+                            input_disturbance.controller_output);
 
 % Define as entradas de texto e ajusta o tamanho da fonte
 hx=xlabel('Tempo (s)');
