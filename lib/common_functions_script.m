a7d1b475561f56eaf8e7c3027cabba2c5edb721c
Mateus Menezes <mateusmenezes95@gmail.com>
2021-09-12 21:08:47 -0300

    Add ramp and sine signals to common functions scripts

--- lib/common_functions_script.m
+++ lib/common_functions_script.m
@@ -5,18 +5,16 @@ pkg load signal
 
 s = tf("s");
 
-default.dt = 0.01;
-default.sim_time = 50;
-
-function sim = get_sim_time (dt = default.dt, end_time = default.sim_time)
+function sim = get_sim_time (dt = 0.01, end_time = 100)
     sim.time_step = dt;
     sim.time = 0:dt:end_time;
+    sim.time = transpose(sim.time);
     if (nargin == 0)
         printf("No arguments passed. Using default values instead\n")
     end
 end
 
-function u = get_step_signal (sim_time, amplitude, start_time, end_time)
+function [u, start_idx, end_idx]  = get_step_signal (sim_time, amplitude, start_time, end_time)
     sim_end_time = sim_time(length(sim_time));
 
     if end_time > sim_end_time
@@ -31,16 +29,41 @@ function u = get_step_signal (sim_time, amplitude, start_time, end_time)
     u(start_idx:end_idx) = amplitude;
 end
 
+function ramp = get_ramp_signal (sim_time, slope, start_time, end_time)
+    [ramp, start_idx, end_idx] = get_step_signal (sim_time, 1, start_time, end_time);
+    ramp(start_idx:end_idx) = (slope * ramp(start_idx:end_idx)
+                               .* (sim_time(start_idx:end_idx) - start_time));
+end
+
+function sine = get_sine_signal (sim_time, amplitude, natural_freq,
+                                 start_time, end_time)
+    [sine, start_idx, end_idx] = get_step_signal (sim_time, 1, start_time, end_time);
+    sine(start_idx:end_idx) = amplitude * sin(natural_freq.*sim_time(start_idx:end_idx));
+end
+
 % function u = get_step_signal (sim_time, param = struct)
 %     u = get_step_signal (sim_time, param.start_time,
 %      param.end_time, param.amplitude)
 % end
 
-function input_signal = get_signal (sim_time, amplitude, start_time, end_time)
+function input_signal = get_signal (sim_time, amplitude,
+                                    start_time, end_time,
+                                    signal = 'step')
     input_signal.start_time = start_time;
     input_signal.end_time = end_time;
-    input_signal.amplitude = amplitude;
-    input_signal.signal = get_step_signal(sim_time, amplitude, start_time, end_time);
+
+    switch (signal)
+        case 'step'
+            input_signal.amplitude = amplitude;
+            input_signal.signal = get_step_signal(sim_time, amplitude,
+                                                  start_time, end_time);
+        case 'ramp'
+            input_signal.slope = amplitude;
+            input_signal.signal = get_ramp_signal(sim_time, amplitude,
+                                                  start_time, end_time);
+        otherwise
+            error('Choose invalid signal! Valids are step or ramp')
+    endswitch
 end
 
 % Y(s)/R(s)
@@ -52,14 +75,14 @@ end
 
 % Y(s)/Qu(s)
 function y_to_qu_tf = get_output_to_input_disturbance_tf (G, C, F)
-    y_to_qu_tf = minreal(F * (G / (1 + (C * G))));
+    y_to_qu_tf = minreal(G / (1 + (C * G)));
     y_to_qu_tf.inname = 'Qu(s)';
     y_to_qu_tf.outname = 'Y(s)';
 end
 
 % Y(s)/Qy(s)
 function y_to_qy_tf = get_output_to_output_disturbance_tf (G, C, F)
-    y_to_qy_tf = minreal(F * (1 / (1 + (C * G))));
+    y_to_qy_tf = minreal(1 / (1 + (C * G)));
     y_to_qy_tf.inname = 'Qy(s)';
     y_to_qy_tf.outname = 'Y(s)';
 end
