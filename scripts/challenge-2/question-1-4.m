36c18f389fefadae82fb3915d522ba0a829adf0d
Mateus Menezes <mateusmenezes95@gmail.com>
2021-09-26 17:50:26 -0300

    Improve graphs texts of questions 1 to 4 results

--- scripts/challenge-2/question-1-4.m
+++ scripts/challenge-2/question-1-4.m
@@ -22,31 +22,48 @@ f = 1;
 % Main of the script
 % =============================================================================
 
-axs = zeros(1,num_plants);
+figure("name", "Incerteza Multiplicativa para cada modelo i")
 
 for i = 1:num_plants
     g(i, :) = k(i) .* exp(-s * l(i)) ./ (tau(i) * s + 1);
     delta(i, :) = abs((g(i, :) - gn) ./ gn);
+
     axs(i) = subplot(num_plants,1,i);
+
     semilogx(w, delta(i, :));
+    grid on
+    set(axs(i), 'ylabel', ['$\Delta_{' num2str(i) '}(jw)$'])
 endfor
 
 lm = max(delta);
-linkaxes(axs);
-ylim([min(lm) 2.5])
+linkaxes(axs,'y');
+ylim([min(lm) 2.5]);
+set(axs(1:3), 'xticklabel', []);
+set(axs(4), 'xlabel', 'Frequência $w$ (rad/s)');
 
 % =============================================================================
 % Plot Graphs
 % =============================================================================
 
+clear axs
+
 figure("name", "Análise da Incerteza Multiplicativa")
-subplot(3,1,1)
+axs(1) = subplot(3,1,1, 'xticklabel', []);
 semilogx(w, lm)
+ylabel('$\bar{\Delta}(w)$')
+grid on
 
 comp_sensibility = (c .* gn) ./ (1 + c .* gn);
-subplot(3,1,2)
+axs(2) = subplot(3,1,2);
 semilogx(w, abs(comp_sensibility))
+ylabel('$|\textit{C}(jw)|$')
+grid on
 
 comp_sensibility_x_lm = abs(comp_sensibility) .* lm;
-subplot(3,1,3)
+axs(3) = subplot(3,1,3);
 semilogx(w, comp_sensibility_x_lm)
+ylabel('$|\textit{C}(jw)|\bar{\Delta}(w)$')
+grid on
+
+set(axs(1:2), 'xticklabel', []);
+set(axs(3), 'xlabel', 'Frequência $w$ (rad/s)');
