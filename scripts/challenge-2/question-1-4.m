b7f7d970d4cb4046dc26747ea0e68948a9804748
Mateus Menezes <mateusmenezes95@gmail.com>
2021-10-02 21:20:51 -0300

    Improve frequency response graphs of challenge 2

--- scripts/challenge-2/question-1-4.m
+++ scripts/challenge-2/question-1-4.m
@@ -28,18 +28,18 @@ for i = 1:num_plants
     g(i, :) = k(i) .* exp(-s * l(i)) ./ (tau(i) * s + 1);
     delta(i, :) = abs((g(i, :) - gn) ./ gn);
 
-    axs(i) = subplot(num_plants,1,i);
+    axs(i) = subplot(2,2,i);
 
     semilogx(w, delta(i, :));
     grid on
     set(axs(i), 'ylabel', ['$\Delta_{' num2str(i) '}(jw)$'])
+    set(axs(i), 'xlabel', 'Frequência $w$ (rad/s)');
 endfor
 
 lm = max(delta);
 linkaxes(axs,'y');
 ylim([min(lm) 2.5]);
 set(axs(1:3), 'xticklabel', []);
-set(axs(4), 'xlabel', 'Frequência $w$ (rad/s)');
 
 % =============================================================================
 % Plot Graphs
@@ -48,22 +48,21 @@ set(axs(4), 'xlabel', 'Frequência $w$ (rad/s)');
 clear axs
 
 figure("name", "Análise da Incerteza Multiplicativa")
-axs(1) = subplot(3,1,1, 'xticklabel', []);
+axs(1) = subplot(2,2,1);
 semilogx(w, lm)
 ylabel('$\bar{\Delta}(w)$')
 grid on
 
 comp_sensibility = (c .* gn) ./ (1 + c .* gn);
-axs(2) = subplot(3,1,2);
+axs(2) = subplot(2,2,2);
 semilogx(w, abs(comp_sensibility))
 ylabel('$|\textit{C}(jw)|$')
 grid on
 
 comp_sensibility_x_lm = abs(comp_sensibility) .* lm;
-axs(3) = subplot(3,1,3);
+axs(3) = subplot(2,2,3:4);
 semilogx(w, comp_sensibility_x_lm)
 ylabel('$|\textit{C}(jw)|\bar{\Delta}(w)$')
 grid on
 
-set(axs(1:2), 'xticklabel', []);
-set(axs(3), 'xlabel', 'Frequência $w$ (rad/s)');
+set(axs, 'xlabel', 'Frequência $w$ (rad/s)');
